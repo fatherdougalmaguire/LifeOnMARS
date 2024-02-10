ff6a3192fcf5b1bfdd0b47fd1d82ab007bd023f9
fatherdougalmaguire <144415904+fatherdougalmaguire@users.noreply.github.com>
2024-02-10 15:28:58 +1100

    Initial Commit

---
+++ LifeOnMars/LifeOnMarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeOnMarsApp.swift
+//  LifeOnMars
+//
+//  Created by Antonio Sanchez-Rivas on 10/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct LifeOnMarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
